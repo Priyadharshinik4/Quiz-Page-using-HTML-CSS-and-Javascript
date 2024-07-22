d805a84f6225061ca6dcef3470d5c69b57beada3
Priyadharshini <priyadharshinik1720@gmail.com>
2024-07-22 17:11:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function b1(){
+    const ans1=document.getElementById("b1");
+    const a1=document.getElementsByClassName("one")[0];
+    a1.style.backgroundColor="red"; 
+}
+function b2(){
+    const ans2=document.getElementById("b2");
+    const a1=document.getElementsByClassName("one")[1];
+    a1.style.backgroundColor="red"; 
+}
+function b3(){
+    const ans3=document.getElementById("b3");
+    const a1=document.getElementsByClassName("one")[2];
+    a1.style.backgroundColor="red"; 
+}
+function b4(){
+    const ans4=document.getElementById("b4");
+    const a1=document.getElementsByClassName("one")[3];
+    a1.style.backgroundColor="green"; 
+}
+
+
